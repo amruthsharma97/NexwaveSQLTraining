1758a8d29012b441fe53037659b2fbd5ed2ab6ea
Amruth B A <amruthsharma97@gmail.com>
2019-12-24 09:35:16 +0530

    third commit

--- day1/ACDB2.sql
+++ day1/ACDB2.sql
@@ -30,3 +30,34 @@ select first_name as "FN",last_name as "LN",monthly_discount as "DC", city||' '|
 select distinct monthly_discount from customers;
 
 select distinct pack_id from customers;
+
+
+select first_name,last_name,pack_id from customers where last_name='King';
+
+
+select * from packages where speed='5Mbps';
+
+select first_name,last_name,pack_id,monthly_discount from customers where monthly_discount<30 ;
+
+
+select join_date from customers where join_date<'01-JAN-07';
+
+select customer_id,first_name,state,city,pack_id from customers where not pack_id=27 and not pack_id=10 and not pack_id=3;
+
+select last_name,main_phone_num,monthly_discount,pack_id from customers where customer_id=703 or customer_id=314 or customer_id=560;
+
+select first_name,monthly_discount from customers where first_name like '%e';
+
+select last_name,pack_id from customers where substr(last_name,2,1)='d';
+
+select * from customers where last_name like '%l%' or last_name like '%j%' or last_name like '%h%' order by monthly_discount desc;
+
+select first_name,join_date,monthly_discount,pack_id from customers WHERE INSTR(LOWER(last_name), 'a') = 0;
+
+select * from customers where pack_id is null;
+
+select first_name||' '||last_name as "Full_Name", monthly_discount from customers where monthly_discount not BETWEEN 20 AND 30 order by "Full_Name";
+
+select first_name||' '||last_name as "Full_Name", main_phone_num||' '||street as "CONTACTS", monthly_discount as "DC" from customers where monthly_discount BETWEEN 11 AND 27;
+
+select * from customers where city='New York' and monthly_discount BETWEEN 30 and 40;
