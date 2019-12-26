bcc37cbd750a67f9ccaa0bc6f83546961ed7d65d
Amruth B A <amruthsharma97@gmail.com>
2019-12-26 16:53:38 +0530

    Forteenth Commit

--- day1/HR2-2.sql
+++ day1/HR2-2.sql
@@ -165,3 +165,25 @@ tblemployees A
 inner join
 (select salary from tblemployees where employee_id = 103) B
 on a.salary<b.salary;
+
+select first_name,salary from tblemployees where salary>(select salary from tblemployees where employee_id = 103);
+
+select department_id,department_name from tbldepartments where location_id=(select location_id from tbldepartments where department_id = 90);
+
+select last_name,hire_date from tblemployees where hire_date>(select hire_date from tblemployees where employee_id = 101);
+
+select first_name,last_name,department_id from tblemployees where department_id=(select department_id from tbldepartments where department_name='Sales');
+
+select department_id,department_name from tbldepartments where location_id=(select location_id from tbllocations where city='Toronto');
+
+select first_name,salary,department_id from tblemployees where department_id=(select department_id from tblemployees where employee_id = 124);
+
+select first_name,salary,department_id from tblemployees where salary>(select avg(salary) from tblemployees);
+
+select first_name,salary,department_id from tblemployees where salary>(select max(salary) from tblemployees where department_id=50);
+
+select first_name,salary,department_id from tblemployees where salary>(select min(salary) from tblemployees where department_id=60);
+
+select first_name,salary,department_id from tblemployees where department_id IN(select department_id from tbldepartments where location_id=(select location_id from tbllocations where city='Seattle'));
+
+select first_name,salary,department_id from tblemployees where salary<(select avg(salary) from tblemployees) and department_id=(select department_id from tblemployees where first_name='Kevin');
